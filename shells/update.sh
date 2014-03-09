4a5151e2eaa8d2756f1f660c2415bef9d429b264
admin <admin@listers.in>
2014-03-09 16:53:43 +0900

    Add update.sh file

--- shells/update.sh
+++ shells/update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-/usr/bin/mongo --host 10.146.14.171 ebay /var/www/listers.in/sql/index.js       > /dev/null
-/usr/bin/mongo --host 10.146.14.171 ebay /var/www/listers.in/sql/summary.js     > /dev/null
-/usr/bin/mongo --host 10.146.14.171 ebay /var/www/listers.in/sql/listeditems.js > /dev/null
-/usr/bin/mongo --host 10.146.14.171 ebay /var/www/listers.in/sql/unanswered.js  > /dev/null
+/usr/bin/mongo --host 10.0.0.57 ebay /var/www/listers.in/sql/index.js       > /dev/null
+/usr/bin/mongo --host 10.0.0.57 ebay /var/www/listers.in/sql/summary.js     > /dev/null
+/usr/bin/mongo --host 10.0.0.57 ebay /var/www/listers.in/sql/listeditems.js > /dev/null
+/usr/bin/mongo --host 10.0.0.57 ebay /var/www/listers.in/sql/unanswered.js  > /dev/null
