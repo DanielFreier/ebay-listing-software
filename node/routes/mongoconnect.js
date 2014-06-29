37a8f0e6727b1e9bcd4371e2b0a849c78fe7a448
admin <admin@listers.in>
2014-06-29 21:23:00 +0900

    Change indentation

--- node/routes/mongoconnect.js
+++ node/routes/mongoconnect.js
@@ -5,26 +5,26 @@ var connectionInstance;
 var config = require('../config');
 
 module.exports = function(callback) {
-  
-  if (connectionInstance) {
-    callback(connectionInstance);
-    return;
-  }
-  
-  var host = config.mongohost;
-  
-  var db = new Db('ebay', 
-                  new Server(host,
-                             Connection.DEFAULT_PORT, 
-                             {
-                               auto_reconnect: true
-                             }));
-  
-  db.open(function(error, databaseConnection) {
-    if (error) throw new Error(error);
     
-    connectionInstance = databaseConnection;
+    if (connectionInstance) {
+        callback(connectionInstance);
+        return;
+    }
     
-    callback(databaseConnection);
-  });
+    var host = config.mongohost;
+    
+    var db = new Db(config.database,
+                    new Server(host,
+                               Connection.DEFAULT_PORT, 
+                               {
+                                   auto_reconnect: true
+                               }));
+    
+    db.open(function(error, databaseConnection) {
+        if (error) throw new Error(error);
+        
+        connectionInstance = databaseConnection;
+        
+        callback(databaseConnection);
+    });
 };
